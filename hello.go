42dd7dcf36e18a068f63a1c45e2f9d8516a84b93
tommitoan <htm1995@gmail.com>
2023-11-06 23:12:21 +0700

    Update/Hello-world-step1

--- hello.go
+++ hello.go
@@ -1,9 +1,26 @@
+//package main
+//
+//import "fmt"
+//
+//func Hello(name string) string {
+//	return "Hello, " + name
+//}
+//
+//func main() {
+//	fmt.Println(Hello("world"))
+//}
+
 package main
 
 import "fmt"
 
+const englishHelloPrefix = "Hello, "
+
 func Hello(name string) string {
-	return "Hello, " + name
+	if name == "" {
+		name = "World"
+	}
+	return englishHelloPrefix + name
 }
 
 func main() {
